29bd09a33087946d6483b7a0473cbe8d72ff9d04
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-06-03 14:25:25 -0300

    FUNCIONOUU O MEU E O DO DIEGO UHUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- fireStoreDB/App.js
+++ fireStoreDB/App.js
@@ -6,14 +6,32 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
 
-  const [nome] = useState('Carregando...');
+  const [nome] = useState('Adicione seus dados...');
 
-  // Defina a função addBancoExterno fora do useEffect
-  async function addBancoExterno() {
-    await setDoc(doc(bancoExterno, "funcionários", "dados"), {
-      TV: "Telefunken",
-      Geladeira: "Continental",
-      Fogão: "Consul"
+  // Função para adicionar dados do primeiro funcionário
+  async function addBancoExterno1() {
+    await setDoc(doc(bancoExterno, "funcionários", "dados1"), {
+      Nome: "L.Barbosa",
+      CPF: "1234567899-x",
+      Profissão: "Soldador"
+    });
+  }
+
+  // Função para adicionar dados do segundo funcionário
+  async function addBancoExterno2() {
+    await setDoc(doc(bancoExterno, "funcionários", "dados2"), {
+      Nome: "Diego",
+      CPF: "167543886-90",
+      Profissão: "Uber"
+    });
+  }
+
+  // Função para adicionar dados do terceiro funcionário
+  async function addBancoExterno3() {
+    await setDoc(doc(bancoExterno, "funcionários", "dados3"), {
+      Nome: "Isaque",
+      CPF: "147567886-78",
+      Profissão: "Professor"
     });
   }
 
@@ -24,9 +42,15 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:25}}>Informação: {nome}</Text>
-      <TouchableOpacity style={{ backgroundColor:"#F50" }} onPress={addBancoExterno}>
-        <Text style={{ fontSize: 20, paddingHorizontal:15 }}>Adicionar</Text>
+      <Text style={{ fontSize: 25 }}>Informação: {nome}</Text>
+      <TouchableOpacity style={styles.button} onPress={addBancoExterno1}>
+        <Text style={styles.buttonText}>Adicionar Dados 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={addBancoExterno2}>
+        <Text style={styles.buttonText}>Adicionar Dados 2</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={addBancoExterno3}>
+        <Text style={styles.buttonText}>Adicionar Dados 3</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
@@ -39,5 +63,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#FFA500', // cor laranja
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10, // adiciona espaço vertical entre os botões
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
   },
 });

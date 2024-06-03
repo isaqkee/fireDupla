38c6d990fb2c456b2faec506dd150804776ad519
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-06-03 13:24:47 -0300

    TA DANDO CERTOO

--- fireStoreDB/App.js
+++ fireStoreDB/App.js
@@ -1,24 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { bancoExterno } from './firebaseConnection';
 import { useEffect, useState } from 'react';
+import { doc, setDoc } from 'firebase/firestore';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
 
-  const [nome, trocaNome] = useState('Carregando...');
+  const [nome] = useState('Carregando...');
+
+  // Defina a função addBancoExterno fora do useEffect
+  async function addBancoExterno() {
+    await setDoc(doc(bancoExterno, "funcionários", "dados"), {
+      TV: "Telefunken",
+      Geladeira: "Continental",
+      Fogão: "Consul"
+    });
+  }
 
   useEffect(() => {
-    async function pegarDados() {
-
-    }
-    pegarDados();
-
-  }, [])
-
+    // Remova a chamada à função pegarDados se ela não estiver definida
+    // pegarDados();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:25}}>Informação:</Text>
+      <Text style={{fontSize:25}}>Informação: {nome}</Text>
+      <TouchableOpacity style={{ backgroundColor:"#F50" }} onPress={addBancoExterno}>
+        <Text style={{ fontSize: 20, paddingHorizontal:15 }}>Adicionar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );

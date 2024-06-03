17eaaf869ec9a84263003b0f02f4f090bffce504
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-06-03 11:22:44 -0300

    começando...

--- fireStoreDB/App.js
+++ fireStoreDB/App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { bancoExterno } from './firebaseConnection';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [nome, trocaNome] = useState('Carregando...');
+
+  useEffect(() => {
+    async function pegarDados() {
+
+    }
+    pegarDados();
+
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize:25}}>Informação:</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ fireStoreDB/firebaseConnection.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKu5kVkjyS5JYtvsk5G_-3WcLmCFckljw",
+  authDomain: "isaquedigas.firebaseapp.com",
+  projectId: "isaquedigas",
+  storageBucket: "isaquedigas.appspot.com",
+  messagingSenderId: "663686181168",
+  appId: "1:663686181168:web:b615e7ccb0272d964298f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
